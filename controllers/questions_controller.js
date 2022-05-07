bab028f6526f33a45d85478761256a70c8dcb376
Benjamin Froehlich <bdfroehlich0@gmail.com>
2022-05-07 15:31:06 -0500

    newQuestion form refactored to use Axios

--- controllers/questions_controller.js
+++ controllers/questions_controller.js
@@ -38,10 +38,10 @@ questions.get('/:email', async (req, res) => {
 questions.post('/', async (req, res) => {
     try {
         const newQuestion = await Question.create(req.body)
-        // res.status(200).json({
-        //     message: 'Successfully inserted a new question.',
-        //     data: newQuestion
-        // })
+        res.status(200).json({
+            message: 'Successfully inserted a new question.',
+            data: newQuestion
+        })
     } catch(err) {
         res.status(500).json(err)
     }
